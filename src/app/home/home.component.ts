e5849e3c2ffdce4f0a0345c953c40bc12ff408fe
Tal Gvili <gviligvili@gmail.com>
2016-12-17 20:29:20 +0200

    second batch

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,49 @@
+import { Component } from '@angular/core';
+
+import { AppState } from '../app.service';
+import { Title } from './title';
+import { XLarge } from './x-large';
+
+@Component({
+  // The selector is what angular internally uses
+  // for `document.querySelectorAll(selector)` in our index.html
+  // where, in this case, selector is the string 'home'
+  selector: 'home',  // <home></home>
+  // We need to tell Angular's Dependency Injection which providers are in our app.
+  providers: [
+    Title
+  ],
+  // Our list of styles in our component. We may add more to compose many styles together
+  styleUrls: ['./home.component.scss'],
+
+
+
+  // Every Angular template is first compiled by the browser before Angular runs it's compiler
+  templateUrl: './home.component.html'
+})
+export class HomeComponent {
+  // Set our default values
+  localState = { value: '' };
+  date: Date = new Date();
+  data: String = "";
+
+  // TypeScript public modifiers
+  constructor(public appState: AppState, public title: Title) {
+
+  }
+
+  ngOnInit() {
+    console.log('hello `Home` component');
+    let harta = [123]
+    console.log("loadash ", _.concat(harta,[3456]));
+    console.log("jquery ", $);
+
+    this.title.getData().subscribe(data => this.data = data.value);
+  }
+
+  submitState(value: string) {
+    console.log('submitState', value);
+    this.appState.set('value', value);
+    this.localState.value = '';
+  }
+}
